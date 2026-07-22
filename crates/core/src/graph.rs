f34df5368050183c9ccf0f0610bcb5fa60c8f601
itay <wo.itay@gmail.com>
2026-07-23 00:45:52 +0300

    feat(core): graph types with bincode roundtrip

---
+++ crates/core/src/graph.rs
@@ -0,0 +1,94 @@
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+pub type NodeId = u32;
+pub type FileId = u32;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub enum DefKind { Function, Method, Class, TestCase, ModuleInit }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub enum EdgeKind { Contains, Calls, Reads, Imports }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Def {
+    pub name: String,
+    pub kind: DefKind,
+    pub file: FileId,
+    pub start_line: u32,
+    pub end_line: u32,
+    pub test_id: Option<Vec<String>>,
+    pub computed_name: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FileNode { pub path: PathBuf, pub hash: [u8; 32], pub lang: String }
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct Graph {
+    pub files: Vec<FileNode>,
+    pub defs: Vec<Def>,
+    pub edges: Vec<(NodeId, EdgeKind, NodeId)>,
+}
+
+impl Graph {
+    pub fn add_file(&mut self, f: FileNode) -> FileId {
+        self.files.push(f);
+        (self.files.len() - 1) as FileId
+    }
+    pub fn add_def(&mut self, d: Def) -> NodeId {
+        self.defs.push(d);
+        (self.defs.len() - 1) as NodeId
+    }
+    pub fn add_edge(&mut self, from: NodeId, kind: EdgeKind, to: NodeId) {
+        self.edges.push((from, kind, to));
+    }
+    pub fn defs_in_file(&self, f: FileId) -> impl Iterator<Item = (NodeId, &Def)> {
+        self.defs.iter().enumerate()
+            .filter(move |(_, d)| d.file == f)
+            .map(|(i, d)| (i as NodeId, d))
+    }
+    pub fn module_init(&self, f: FileId) -> Option<NodeId> {
+        self.defs_in_file(f).find(|(_, d)| d.kind == DefKind::ModuleInit).map(|(i, _)| i)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+
+    fn f(path: &str) -> FileNode {
+        FileNode { path: PathBuf::from(path), hash: [0; 32], lang: "ts".into() }
+    }
+    fn d(name: &str, kind: DefKind, file: FileId) -> Def {
+        Def { name: name.into(), kind, file, start_line: 1, end_line: 2, test_id: None, computed_name: false }
+    }
+
+    #[test]
+    fn add_and_query() {
+        let mut g = Graph::default();
+        let fa = g.add_file(f("a.ts"));
+        let fb = g.add_file(f("b.ts"));
+        let init = g.add_def(d("<module>", DefKind::ModuleInit, fa));
+        let add = g.add_def(d("add", DefKind::Function, fa));
+        let other = g.add_def(d("x", DefKind::Function, fb));
+        g.add_edge(init, EdgeKind::Contains, add);
+
+        assert_eq!(g.defs_in_file(fa).count(), 2);
+        assert_eq!(g.defs_in_file(fb).map(|(id, _)| id).collect::<Vec<_>>(), vec![other]);
+        assert_eq!(g.module_init(fa), Some(init));
+        assert_eq!(g.module_init(fb), None);
+    }
+
+    #[test]
+    fn roundtrips_through_bincode() {
+        let mut g = Graph::default();
+        let fa = g.add_file(f("a.ts"));
+        g.add_def(d("add", DefKind::Function, fa));
+        let bytes = bincode::serialize(&g).unwrap();
+        let g2: Graph = bincode::deserialize(&bytes).unwrap();
+        assert_eq!(g2.defs.len(), 1);
+        assert_eq!(g2.files[0].path, PathBuf::from("a.ts"));
+    }
+}
